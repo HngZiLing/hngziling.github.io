aa6fd87fe2d3542482d34a7e523b60ceae166da8
H'ng Zi Ling <72725903+HngZiLing@users.noreply.github.com>
2022-12-21 23:25:48 +0800

    Add files via upload

---
+++ stiw3054-assignment-2/JavaDoc/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes", "u": "allclasses-index.html"}, {"p": "my.uum", "l": "App"}, {
+    "p": "my.uum",
+    "l": "s281895_A221_bot"
+}, {"p": "my.uum", "l": "SQLite"}];
+updateSearchResults();
